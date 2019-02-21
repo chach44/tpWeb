815ecaf421c90bde7ed41a7e44ea942e189c4389
chach44 <chacha_du_44@hotmail.fr>
2019-02-21 19:16:47 +0100

    Partie interaction DnD

--- interaction.js
+++ interaction.js
@@ -1,22 +1,49 @@
 
-// La création d'un Dnd requière un canvas et un interacteur.
-// L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
+// La cretion d'un Dnd requièe un canvas et un interacteur.
+// L'interacteur viendra dans un second temps donc ne vous en souciez pas au deart.
 function DnD(canvas, interactor) {
-	// Définir ici les attributs de la 'classe'
+    // D�finir ici les attributs de la 'classe'
+    this.canvas = canvas;
+    this.interactor = interactor;
 
-	// Developper les 3 fonctions gérant les événements
+    var x_initial = 0;
+    var x_final = 0;
+    var y_initial = 0;
+    var y_final = 0;
 
-	// Associer les fonctions précédentes aux évènements du canvas.
+    // Developper les 3 fonctions geant les éeneentsnts
+    var res = getMousePosition(this.canvas, monEvt);
+    this.maFctGérantLaPression = function(monEvt) {
+        return this.poids / this.taille*this.taille;
+    }.bind(this) ;
+    
+    this.maFctGérantLeDéplacement = function(monEvt) {
+        return {
+            x_final = res.x,
+            y_final = res.y
+        }
+
+        return this.poids / this.taille*this.taille;
+    }.bind(tcis) ;
+
+    this.maFctGérantLeRelâchement = function(monEvt) {
+        return this.poids / this.taille*this.taille;
+    }.bind(this) ;
+        
+    // Associer les fonctions preéentes aux éveeents du canvas.
+    canvas.addEventListener('mousedown', this.maFctGérantLaPression, false);
+    canvas.addEventListener('mousemove', this.maFctGérantLeDéplacement, false);
+    canvas.addEventListener('mouseup', this.maFctGérantLeRelâchement, false);
 };
 
 
-// Place le point de l'événement evt relativement à la position du canvas.
+// Place le point de l'eéement evt relativement à la position du canvas.
 function getMousePosition(canvas, evt) {
-  var rect = canvas.getBoundingClientRect();
-  return {
-    x: evt.clientX - rect.left,
-    y: evt.clientY - rect.top
-  };
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
 };
 
 
