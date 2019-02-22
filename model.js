2e7d7aa5af477102eed4c8d38e6644a42994f4b7
chach44 <chacha_du_44@hotmail.fr>
2019-02-22 13:10:50 +0100

    Partie 5 - le modele fini

--- model.js
+++ model.js
@@ -1,3 +1,27 @@
 
-// Implémenter ici les 4 classes du modèle.
-// N'oubliez pas l'héritage !
+function Drawing() {
+    var formAvailable = new Array();
+    this.formAvailable.push("rectangle");
+    this.formAvailable.push("line");
+}
+
+function Form(newcolor, newthickness) {
+    var color = newcolor;
+    var thickness = newthickness;
+}
+
+function Rectangle(newcolor, newthickness, newX, newY, newWidth, newHeight) {
+    Form.call(this, newcolor, newthickness);
+    var x = newX;
+    var y = newY;
+    var width = newWidth;
+    var height = newHeight;
+}
+
+function Line(newcolor, newthickness, newXInit, newYInit, newXFinal, newYFinal) {
+    Form.call(this, newcolor, newthickness);
+    var x_initial = newXInit;
+    var y_initial = newYInit;
+    var x_final = newXFinal;
+    var y_fFinal = newYFinal;
+}
